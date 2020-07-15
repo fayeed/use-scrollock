8d6b4d23338a819535877a10ff0bf0648e3de985
Fayeed <fayeed@live.com>
2020-07-15 17:05:54 +0530

    Hook added

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,69 @@
-import * as React from 'react';
+import { useEffect, useRef, useState, MutableRefObject } from "react";
 
-export const useMyHook = () => {
-  let [{
-    counter
-  }, setState] = React.useState<{
-    counter: number;
-  }>({
-    counter: 0
-  });
+interface UseScrollockProps {
+  disableHorizontalScroll?: boolean;
+  disableVerticalScroll?: boolean;
+  padScrollbarSpace?: boolean;
+  ref?: MutableRefObject<HTMLElement | undefined>;
+}
 
-  React.useEffect(() => {
-    let interval = window.setInterval(() => {
-      counter++;
-      setState({counter})
-    }, 1000)
-    return () => {
-      window.clearInterval(interval);
-    };
-  }, []);
+interface UseScrollockReturntype {
+  scrollock: boolean;
+  toggleScrollock: (value?: boolean) => void;
+}
 
-  return counter;
+export const useScrollock = (
+  options: UseScrollockProps
+): UseScrollockReturntype => {
+  const {
+    ref,
+    disableHorizontalScroll = true,
+    disableVerticalScroll = true,
+    padScrollbarSpace = false,
+  } = options || {};
+  const [scrollock, setScrollock] = useState(false);
+
+  let innerRef = useRef<HTMLElement>();
+
+  const toggleScrollock = (value?: boolean) =>
+    setScrollock(value ? value : !scrollock);
+
+  useEffect(() => {
+    innerRef.current = ref?.current
+      ? ref?.current
+      : document.getElementsByTagName("body")![0];
+  }, [ref]);
+
+  useEffect(() => {
+    let body = innerRef.current!;
+    body.style.paddingRight = !padScrollbarSpace
+      ? "0px"
+      : `${!scrollock ? 0 : window.innerWidth - body.offsetWidth}px`;
+
+    body.style.overflowY =
+      scrollock && disableVerticalScroll ? "hidden" : "auto";
+    body.style.overflowX =
+      scrollock && disableHorizontalScroll ? "hidden" : "auto";
+
+    if (scrollock) {
+      body.addEventListener("touchmove", disableTouch, { passive: false });
+      body.addEventListener("scroll", disableScroll, { passive: false });
+    } else {
+      body.removeEventListener("touchmove", disableTouch);
+      body.removeEventListener("scroll", disableScroll);
+    }
+  }, [scrollock]);
+
+  return { scrollock, toggleScrollock };
+};
+
+const disableScroll = (e: Event) => {
+  e.preventDefault();
+  return false;
+};
+
+const disableTouch = (e: TouchEvent) => {
+  if (e.touches.length) return true;
+  e.preventDefault();
+  return false;
 };
